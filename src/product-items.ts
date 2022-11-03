cedf6a0b35a272462df6091e03f235ef38ec6477
yaaHosein <layali.14.peshawar@hotmail.com>
2022-11-03 15:09:47 +0330

    making shopping cart

--- src/product-items.ts
+++ src/product-items.ts
@@ -1,1 +1,19 @@
-export class ProductItems{}
+export class ProductItems {
+  products = [
+    {
+      id: 1,
+      name: "foo",
+      price: 99.99,
+    },
+    {
+      id: 2,
+      name: "baz",
+      price: 33.99,
+    },
+    {
+      id: 3,
+      name: "nono",
+      price: 44.99,
+    },
+  ];
+}
