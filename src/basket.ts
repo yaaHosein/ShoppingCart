06d927c3c5a3d54f99b739c5d6444cc49aa3bdc6
yaaHosein <layali.14.peshawar@hotmail.com>
2022-11-06 12:46:47 +0330

    adding ./

--- src/basket.ts
+++ src/basket.ts
@@ -1,11 +1,11 @@
-import { BasketService } from "services/basket-service";
+import { BasketService } from "./services/basket-service";
 import { inject } from "aurelia-framework";
 @inject(BasketService)
 export class Basket {
   basketservice: BasketService;
-basket(){
-    return this.basketservice.basket
-}
+  basket() {
+    return this.basketservice.basket;
+  }
   constructor(basketSrvs) {
     this.basketservice = basketSrvs;
   }
