a7e01a2d723716d80b2af6c126c9ad360b7ca5aa
yaaHosein <layali.14.peshawar@hotmail.com>
2022-11-08 15:49:43 +0330

    trying to show the items length in the basket view near the basket view button

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,26 @@ import { BasketService } from "./services/basket-service";
 import { inject } from "aurelia-framework";
 
 type View = "products" | "basket";
-@inject(BasketService)
+
 export class App {
   basketservice: BasketService;
   activeView: View = "products";
-  me: View = "basket";
   setActiveView(view: View) {
     this.activeView = view;
   }
 
-  mm(view: View) {
-    this.me = view;
-  }
+  //   addToBaskett() {
+  //     this.basketservice.basket.length;
+  //   }
+}
+@inject(BasketService)
+export class Basket {
+  basketservice: BasketService;
+  basket;
 
-  addToBaskett() {
-    this.basketservice.basket.length;
+  constructor(basketSrvs) {
+    this.basketservice = basketSrvs;
+    this.basket = this.basketservice.basket;
   }
 }
+
