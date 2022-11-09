4d031322e80cb54dd691c8650ac1ea9f54c32567
yaaHosein <layali.14.peshawar@hotmail.com>
2022-11-09 04:24:04 +0330

    removing the wrong codes

--- src/app.ts
+++ src/app.ts
@@ -3,28 +3,11 @@ import { inject } from "aurelia-framework";
 
 type View = "products" | "basket";
 
+@inject(BasketService)
 export class App {
   basketservice: BasketService;
   activeView: View = "products";
   setActiveView(view: View) {
     this.activeView = view;
   }
-
-  //   addToBaskett() {
-  //     this.basketservice.basket.length;
-  //   }
 }
-@inject(BasketService)
-export class Basket {
-  basketservice: BasketService;
-  basket;
-
-  constructor(basketSrvs) {
-    this.basketservice = basketSrvs;
-    this.basket = this.basketservice.basket;
-  }
-  basketLength(product) {
-    this.basketservice.add(product);
-  }
-}
-
