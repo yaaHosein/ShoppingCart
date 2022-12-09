65249406bfc21d76b5afba7b9565bf879c42f703
yaaHosein <layali.14.peshawar@hotmail.com>
2022-12-09 20:22:26 +0330

    making Super TODos List

---
+++ src/app.js
@@ -0,0 +1,16 @@
+export class App {
+  constructor() {
+    this.toDos = [];
+    this.toDoDescription = "";
+  }
+
+  addToDo() {
+    this.toDos.push({ description: this.toDoDescription });
+    this.toDoDescription = "";
+  }
+
+  removeToDo(toDo) {
+    let index = this.toDos.indexOf(toDo);
+    this.toDos.splice(index);
+  }
+}
--- src/types/product-types.ts
+++
@@ -1,6 +0,0 @@
-export type Product = {
-  name: string;
-  id: number;
-  price: number;
-  category: string;
-};
